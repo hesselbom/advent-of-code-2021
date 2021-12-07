53c81d78388c3150b4793234c52e5fb7ff2a8565
Viktor Hesselbom <viktor@hesselbom.net>
2021-12-07 09:54:18 +0100

    day 7 part 2

---
+++ day7-part2.js
@@ -0,0 +1,19 @@
+module.exports = (input) => {
+  const crabs = input.split(',').map(n => parseInt(n))
+  const maxStartX = Math.max(...crabs)
+  let lowest = Infinity
+
+  for (let x = 0; x <= maxStartX; x++) {
+    let total = crabs.reduce((tot, crab) => (
+      tot + triangleNumber(Math.abs(crab - x))
+    ), 0)
+
+    lowest = Math.min(total, lowest)
+  }
+
+  return lowest
+}
+
+function triangleNumber(n) {
+  return (n * (n + 1)) / 2
+}
