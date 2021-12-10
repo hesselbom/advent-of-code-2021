c106186f75b77aca28fcfe60ed5fc24d04ac3c48
Viktor Hesselbom <viktor@hesselbom.net>
2021-12-10 10:54:30 +0100

    day 10 part 1

---
+++ day10-part1.js
@@ -0,0 +1,52 @@
+const findCloser = (row, state, expected) => {
+  let char = row.charAt(state.index)
+
+  while (char) {
+    if (char === expected) return true
+
+    if (closers.includes(char)) return char
+
+    if (openers.includes(char)) {
+      state.index += 1
+      const result = findCloser(row, state, closerMap[char])
+      if (result !== true) {
+        return result
+      }
+    }
+
+    state.index += 1
+    char = row.charAt(state.index)
+  }
+
+  return false
+}
+
+const openers = ['(', '[', '{', '<']
+const closers = [')', ']', '}', '>']
+const closerMap = {
+  '(': ')',
+  '[': ']',
+  '{': '}',
+  '<': '>'
+}
+const pointsMap = {
+  ')': 3,
+  ']': 57,
+  '}': 1197,
+  '>': 25137
+}
+
+module.exports = (input) => {
+  const rows = input.split('\n')
+  const row = '{([(<{}[<>[]}>{[]{[(<()>'
+  let count = 0
+
+  for (let row of rows) {
+    const firstChar = row.charAt(0)
+    const result = findCloser(row, { index: 1 }, closerMap[firstChar])
+
+    count += pointsMap[result] || 0
+  }
+
+  return count
+}
