ff757b795e939e5770163afd86477a336472dd9f
Viktor Hesselbom <viktor@hesselbom.net>
2021-12-11 10:42:01 +0100

    day 11 part 1

---
+++ day11-part1.js
@@ -0,0 +1,46 @@
+const add = (y, x, rows, flashed) => {
+  if (y < 0 || x < 0 || y > 9 || x > 9) return
+
+  rows[y][x] += 1
+
+  if (rows[y][x] > 9) {
+    const key = y + ',' + x
+    if (flashed.has(key)) return
+
+    // flash
+    flashed.add(key)
+
+    add(y - 1, x - 1, rows, flashed)
+    add(y - 1, x, rows, flashed)
+    add(y - 1, x + 1, rows, flashed)
+    add(y, x - 1, rows, flashed)
+    add(y, x + 1, rows, flashed)
+    add(y + 1, x - 1, rows, flashed)
+    add(y + 1, x, rows, flashed)
+    add(y + 1, x + 1, rows, flashed)
+  }
+}
+
+module.exports = (input) => {
+  const rows = input.split('\n').map(r => r.split('').map(v => parseInt(v)))
+  const steps = 100
+  let flashes = 0
+
+  for (let i = 0; i < steps; i++) {
+    const flashed = new Set()
+
+    for (let y = 0; y < 10; y++) {
+      for (let x = 0; x < 10; x++) {
+        add(y, x, rows, flashed)
+      }
+    }
+
+    flashes += flashed.size
+    for (let key of flashed) {
+      const [y, x] = key.split(',')
+      rows[y][x] = 0
+    }
+  }
+
+  return flashes
+}
